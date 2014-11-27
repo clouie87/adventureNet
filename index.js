fbcb516a3fe8cccf4f6575e650ee307fb069bdcc
clouie87 <carol.louie@gmail.com>
2014-11-27 11:48:49 -0400

    working on creating elements and validating inputs

--- index.js
+++ index.js
@@ -5,13 +5,27 @@ var app = express();// originally var app = require('express')(); which i think 
 var http = require('http').Server(app);
 var io= require('socket.io')(http); //after downloading the socket.io we need to state the var we initialize the (socket.io)
 //and pass it through the http object\
+//var redis = require('redis');
+//
+//var client = redis.createClient();
+//
+//client.on("error", function (err) {
+//	console.log("Error " + err);
+//});
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/', function (req, res){
+	res.send('Welcome');
+});
+
+app.use('/styles', express.static(__dirname +'/styles'));
 app.use(express.static(__dirname +'/public'));
+
 
 http.listen(3000, function(){
 	console.log('listening on *:3000...');
-}); 
+});
+
