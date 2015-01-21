7657c85328ea2125904c151f3d3cc36ee036d59e
clouie87 <carol.louie@gmail.com>
2015-01-20 23:29:11 -0400

    facebook login button

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 //resource: http://expressjs.com/4x/api.html#application
 
-var express = require('express');
-var app = express();// originally var app = require('express')(); which i think sums up line 3 and 4 but we need the var express if i want to add in my own js. 
+
+var express = require('express'),
+	exphbs = require('express-handlebars'),
+	logger = require('morgan'),
+	cookieParser = require('cookie-parser'),
+	bodyParser = require('body-parser'),
+	methodOverride = require('method-override'),
+	session = require('express-session'),
+	passport = require('passport'),
+	LocalStrategy = require('passport-local'),
+	TwitterStrategy = require('passport-twitter'),
+	GoogleStrategy = require('passport-google'),
+	FacebookStrategy = require('passport-facebook').Strategy;
+
+
+var app = express();// originally var app = require('express')(); which i think sums up line 3 and 4 but we need the var express if i want to add in my own js.
 var http = require('http').Server(app);
 var io= require('socket.io')(http); //after downloading the socket.io we need to state the var we initialize the (socket.io)
 //and pass it through the http object\
+
+app.use('/styles', express.static(__dirname +'/styles'));
+app.use(express.static(__dirname +'/public'));
+
+
 var pg = require('pg');
+
+var userCount = 0;
+
+var FACEBOOK_APP_ID ='295897893929835';
+var FACEBOOK_APP_SECRET = '016c48478907f77a428e2dfb5724edf7';
+
+//============== POSTGRES  STUFF ====================//
 
 var conString = "postgres://carolinelouie@localhost/navigatio";
 
+//postgres client set up
 var client = new pg.Client(conString);
 client.connect(function(err) {
 	if(err) {
@@ -26,18 +53,184 @@ client.connect(function(err) {
 	});
 });
 
-var userCount = 0;
+//============== END OF POSTGRES  STUFF (EXCEPT FOR WHATS IN SOCKET STUFF)====================//
 
+var config = require('./config.js'), //config file contains all tokens and other private info
+	funct = require('./functions.js');
+
+//============== EXPRESS STUFF ====================//
+
+// Configure Express
+app.use(logger('combined'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Session-persisted message middleware
+app.use(function(req, res, next){
+	var err = req.session.error,
+		msg = req.session.notice,
+		success = req.session.success;
+
+	delete req.session.error;
+	delete req.session.success;
+	delete req.session.notice;
+
+	if (err) res.locals.error = err;
+	if (msg) res.locals.notice = msg;
+	if (success) res.locals.success = success;
+
+	next();
+});
+
+// Configure express to use handlebars templates
+var hbs = exphbs.create({
+	defaultLayout: 'main', //we will be creating this layout shortly
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+//===============PASSPORT  STUFF ===================//
+// Passport session setup.
+passport.serializeUser(function(user, done) {
+	console.log("serializing " + user.username);
+	done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+	console.log("deserializing " + obj);
+	done(null, obj);
+});
+
+// Use the LocalStrategy within Passport to login/”signin” users.
+passport.use('local-signin', new LocalStrategy(
+	{passReqToCallback : true}, //allows us to pass back the request to the callback
+	function(req, username, password, done) {
+		funct.localAuth(username, password)
+			.then(function (user) {
+				if (user) {
+					console.log("LOGGED IN AS: " + user.username);
+					req.session.success = 'You are successfully logged in ' + user.username + '!';
+					done(null, user);
+				}
+				if (!user) {
+					console.log("COULD NOT LOG IN");
+					req.session.error = 'Could not log user in. Please try again.'; //inform user could not log them in
+					done(null, user);
+				}
+			})
+			.fail(function (err){
+				console.log(err.body);
+			});
+	}
+));
+// Use the LocalStrategy within Passport to register/"signup" users.
+passport.use('local-signup', new LocalStrategy(
+	{passReqToCallback : true}, //allows us to pass back the request to the callback
+	function(req, username, password, done) {
+		funct.localReg(username, password)
+			.then(function (user) {
+				if (user) {
+					console.log("REGISTERED: " + user.username);
+					req.session.success = 'You are successfully registered and logged in ' + user.username + '!';
+					done(null, user);
+				}
+				if (!user) {
+					console.log("COULD NOT REGISTER");
+					req.session.error = 'That username is already in use, please try a different one.'; //inform user could not log them in
+					done(null, user);
+				}
+			})
+			.fail(function (err){
+				console.log(err.body);
+			});
+	}
+));
+
+//var User = '';
+//
+//passport.use(new FacebookStrategy({
+//		clientID: FACEBOOK_APP_ID,
+//		clientSecret: FACEBOOK_APP_SECRET,
+//		callbackURL: 'http://localhost:3000/auth/facebook/callback'
+//
+//	},
+//	function(accessToken, refreshToken, profile, done) {
+//		User.findOrCreate(profile, function(err, user) {
+//		if (err) { return done(err); }
+//		done(null, user);
+//	});
+//	}
+//));
+
+//================= ROUTES STUFF ====================//
+
+//displays our homepage
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+	res.render('home', {user: req.user});
 });
 
-app.get('/', function (req, res){
-	res.send('Welcome');
+//displays our signup page
+app.get('/signin', function(req, res){
+	res.render('signin');
 });
 
-app.use('/styles', express.static(__dirname +'/styles'));
-app.use(express.static(__dirname +'/public'));
+////displays our signup page
+//app.get('/homepage', function(req, res){
+//	res.render('homepage');
+//});
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+	passport.authenticate('facebook', { successRedirect: '/',
+		failureRedirect: '/login' }));
+
+//app.get('/auth/facebook',
+//	passport.authenticate('facebook'), //{ scope: 'public_profile,email' }
+//);
+
+//GET /oauth/access_token?
+//	client_id=
+//	&client_secret={app-secret}
+//	&grant_type=client_credentials
+
+
+
+
+
+//sends the request through our local signup strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/local-reg', passport.authenticate('local-signup', {
+		successRedirect: '/',
+		failureRedirect: '/signin'
+	})
+);
+
+//sends the request through our local login/signin strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/login', passport.authenticate('local-signin', {
+		successRedirect: '/',
+		failureRedirect: '/signin'
+	})
+);
+
+//logs user out of site, deleting them from the session, and returns to homepage
+app.get('/logout', function(req, res){
+	var name = req.user.username;
+	console.log("LOGGIN OUT " + req.user.username);
+	req.logout();
+	res.redirect('/');
+	req.session.notice = "You have successfully been logged out " + name + "!";
+});
+
+
+
+
+//============== SOCKET STUFF ====================//
 
 io.on('connection', function(socket) { // then listen to the connection event for incoming sockets
 	console.log("connnected");
@@ -108,6 +301,8 @@ io.on('connection', function(socket) { // then listen to the connection event fo
 	//	//socket.emit('username', username);
 	//	socket.emit("users",usernames);
 	//});
+
+//========================= END OF SOCKET STUFF ==============================//
 
 
 http.listen(3000, function(){
